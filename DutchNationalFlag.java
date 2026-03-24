270fba6cc6ebe52a064726b857cbb0be4c278f3a
gurushantmattibounteous <gurushant.matti@bounteous.com>
2026-03-24 11:18:42 +0530

    dutchnational flag

---
+++ DutchNationalFlag.java
@@ -0,0 +1,37 @@
+public class DutchNationalFlag {
+
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else { // nums[mid] == 2
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        DutchNationalFlag dnf = new DutchNationalFlag();
+
+        int[] nums = { 2, 0, 2, 1, 1, 0 };
+
+        dnf.sortColors(nums);
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
