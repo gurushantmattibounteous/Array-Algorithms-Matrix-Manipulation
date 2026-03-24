b0bc69384bcfd17289e76ca955b8e13e55b9760b
gurushantmattibounteous <gurushant.matti@bounteous.com>
2026-03-24 13:52:28 +0530

    roatebinary

---
+++ roatebs.java
@@ -0,0 +1,41 @@
+public class roatebs {
+
+    static class Solution {
+        public int search(int[] nums, int target) {
+            int lo = 0;
+            int hi = nums.length - 1;
+
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+
+                if (nums[mid] == target)
+                    return mid;
+
+                if (nums[lo] <= nums[mid]) {
+                    if (target >= nums[lo] && target < nums[mid]) {
+                        hi = mid - 1;
+                    } else {
+                        lo = mid + 1;
+                    }
+                } else {
+                    if (target > nums[mid] && target <= nums[hi]) {
+                        lo = mid + 1;
+                    } else {
+                        hi = mid - 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+        int target = 0;
+
+        int result = sol.search(nums, target);
+        System.out.println("Index: " + result);
+    }
+}
